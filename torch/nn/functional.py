92d67ad71afcffd7c53ac2d387ab1dceff9313c7
aritra1804 <aritradutta04@gmail.com>
2025-09-10 19:27:57 -0400

    Add documentation note to conv1d function in main branch

--- torch/nn/functional.py
+++ torch/nn/functional.py
@@ -46,6 +46,8 @@ conv1d(input, weight, bias=None, stride=1, padding=0, dilation=1, groups=1) -> T
 Applies a 1D convolution over an input signal composed of several input
 planes.
 
+Note: This is a main branch modification for merge conflict demonstration.
+
 {tf32_note}
 
 See :class:`~torch.nn.Conv1d` for details and output shape.
